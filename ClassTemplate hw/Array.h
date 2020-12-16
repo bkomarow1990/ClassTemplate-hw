3368eeb7d04840209b89af260fa04738dc51265c
Bogdan <71147719+bkomarow1990@users.noreply.github.com>
2020-12-16 12:52:14 +0200

    Added HW

--- ClassTemplate hw/Array.h
+++ ClassTemplate hw/Array.h
@@ -12,8 +12,23 @@ private:
 public:
 	Array(const size_t & grow,const size_t& size);
 	Array();
+	~Array();
 	const int getSize()const;
 	void print()const;
+	void setSize(const size_t& size, const size_t grow = 1);
+	void add(const T& data);
+	const int getUpperBound()const;
+	const bool isEmpty()const;
+	void freeExtra();
+	void removeAll();
+	const T getAt(const size_t& index);
+	void setAt(const int& index, const T& elem);
+	int& operator[](int index);
+	void concat(const Array& other);
+	Array& operator=(const Array& other);
+	void showAdressOfArray()const;
+	void insertAt(const int& index, const T& elem);
+	void removeAt(const int& index);
 };
 
 
@@ -23,16 +38,26 @@ template<typename T>
 inline Array<T>::Array(const size_t& grow, const size_t& size)
 	:size(size)
 {
+	arr = new T[size];
 	if (grow!=0)
 	{
+		this->grow = grow;
+	}
+	else {
 		this->grow = grow;
 	}
 }
 
 template<typename T>
 inline Array<T>::Array()
-	:grow(1),size(0)
+	:Array(1,0)
 {
+}
+
+template<typename T>
+inline Array<T>::~Array()
+{
+	removeAll();
 }
 
 template<typename T>
@@ -44,9 +69,9 @@ inline const int Array<T>::getSize() const
 template<typename T>
 inline void Array<T>::print() const
 {
-	if (size!=0)
+	if (top!=-1)
 	{
-		for (size_t i = 0; i < size; i++)
+		for (int i = 0; i <= top; i++)
 		{
 			cout << arr[i] << " ";
 		}
@@ -54,3 +79,207 @@ inline void Array<T>::print() const
 	}
 	
 }
+
+template<typename T>
+inline void Array<T>::setSize(const size_t& size, const size_t grow)
+{
+	this->grow = grow;
+	if (this->size<size)
+	{
+		T* tmp = arr;
+		this->size = size; 
+		arr = new T[size];
+		for (size_t i = 0; i < top+1; i++)
+		{
+			arr[i] = tmp[i];
+		}
+		delete[]tmp;
+		return;
+	}
+	else if (this->size>size && top!=-1) {
+		T *tmp = arr;
+		this->size = size;
+		arr = new T[size];
+		// 10 23 40 60 * * size=6 top=3
+		// 
+		// 10 23 * * * * size=2 top=3
+		top=size-1;
+		for (int i = 0; i <= top; i++)
+		{
+			arr[i] = tmp[i];
+		}
+		
+		delete[]tmp;
+	}
+}
+
+template<typename T>
+inline void Array<T>::add(const T& data)
+{
+	T* tmp = new T[size];
+	tmp = arr;
+	size += grow+1;
+	++top;
+	arr = new T[size];
+	if (top!=0)
+	{
+	for (int i = 0; i < top; i++)
+	{
+		arr[i] = tmp[i];
+	}
+	}
+	arr[top] = data;
+	delete[]tmp;
+}
+
+template<typename T>
+inline const int Array<T>::getUpperBound() const
+{
+	return top;
+}
+
+template<typename T>
+inline const bool Array<T>::isEmpty() const
+{
+	return top==-1;
+}
+
+template<typename T>
+inline void Array<T>::freeExtra()
+{
+	setSize(top + 1, grow);
+}
+
+template<typename T>
+inline void Array<T>::removeAll()
+{
+	top = -1;
+	arr = nullptr;
+	delete[]arr;
+}
+
+template<typename T>
+inline const T Array<T>::getAt(const size_t& index)
+{
+	if (!isEmpty() && index>=0 && index<=top)
+	{
+		
+		return arr[index];
+	}
+	cout << "Enter correct index" << endl;
+	return 0;
+}
+
+template<typename T>
+inline void Array<T>::setAt(const int& index, const T& elem)
+{
+	if (isEmpty() || index >= size)
+	{
+		cout << "Enter correct index" << endl;
+		return;
+	}
+	arr[index] = elem;
+}
+
+template<typename T>
+inline int& Array<T>::operator[](int index)
+{
+	
+		static int error;
+		if (!isEmpty() && index >= 0 && index <= top)
+		{
+			return arr[index];
+		}
+		cerr << "Error index" << index << endl; // cerr = stream for errors
+		return error;
+
+}
+
+template<typename T>
+inline void Array<T>::concat(const Array& other)
+{
+	if (isEmpty() || other.isEmpty())
+	{
+		cout << "One of array is empty" << endl;
+		return;
+	}
+	T* tmp = new T[(top + 1 )+ (other.top + 1)];
+	for (int i = 0; i < top+1; i++)
+	{
+		tmp[i] = arr[i];
+	}
+	int j = 0;
+	for (int i = top+1; i < (top+1)+(other.top+1); i++)
+	{
+		tmp[i] = other.arr[j];
+		++j;
+	}
+	top = this->top + other.top+1;
+	arr = new T[(top + 1) + (other.top + 1)];
+	arr = tmp;
+	tmp = nullptr;
+	delete[]tmp;
+}
+
+template<typename T>
+inline Array<T>& Array<T>::operator=(const Array& other)
+{
+
+	size = other.size;
+	grow = other.grow;
+	top = other.top;
+	for (size_t i = 0; i < top+1; i++)
+	{
+		arr[i] = other.arr[i];
+	}
+	
+	return *this;
+}
+
+template<typename T>
+inline void Array<T>::showAdressOfArray() const
+{
+	std::cout << &arr << endl;
+}
+
+template<typename T>
+inline void Array<T>::insertAt(const int& index, const T& elem)
+{
+	if (isEmpty() || index > top || index<0)
+	{
+		cout << "Enter correct index" << endl;
+		return;
+	}
+	arr[index] = elem;
+
+}
+
+template<typename T>
+inline void Array<T>::removeAt(const int& index)
+{
+	if (!isEmpty())
+	{
+	if (index==top || top==0)
+	{
+		--top;
+		freeExtra();
+		return;
+	}
+		T* tmp = arr;
+		--size;
+		--top;
+		arr = new T[size];
+		int i = 0;
+		while (i != index) {
+			arr[i] = tmp[i];
+			++i;
+		}
+		while (i!=top+1)
+		{
+			arr[i] = tmp[i+1];
+			++i;
+		}
+	}
+}
+
+
--- ClassTemplate hw/Source.cpp
+++ ClassTemplate hw/Source.cpp
@@ -2,7 +2,51 @@
 #include "Array.h"
 using namespace std;
 int main() {
-	Array<int>one(1,1);
+	Array<int>one(3,5);
+	one.print();
+	one.add(4);
+	one.add(5);
+	one.add(6);
+	one.add(6);
+	one.add(6);
+	one.print();
+	one.setSize(3, 3);
+	cout << "test" << endl;
+	one.print();
+	cout << "getAt: " << endl;
+	cout<<one.getAt(2)<<endl;
+	one.setAt(2, 12);
+	cout<<one.getAt(2)<<endl;
+	cout << "operator[]: " << endl;
+	cout << one[2] << endl;
+	cout << "Concat: " << endl;
+	Array<int> two(3,5);
+	cout << "One: " << endl;
+	one.print();
+	cout << "Two: " << endl;
+	two.add(3);
+	two.add(6);
+	two.add(8);
+	two.print();
+	cout << "After: " << endl;
+	
+	one.concat(two);
+	one.print();
+	cout << "Operator =" << endl;
+	cout << "Before:" << endl;
+	one.print();
+	two.print();
+	cout << "After: " << endl;
+	two = one;
+	one.print();
+	two.print();
+	one.showAdressOfArray();
+	two.showAdressOfArray();
+	cout << "Insert at: " << endl;
+	one.insertAt(3, 228);
+	one.print();
+	cout << "Temove at: " << endl;
+	one.removeAt(3);
 	one.print();
 	return 0;
 }
